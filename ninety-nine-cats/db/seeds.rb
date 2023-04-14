26918f60fe3ce305657177be33616be976fa6068
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-04-14 17:03:19 -0400

    seed data, views for index

--- ninety-nine-cats/db/seeds.rb
+++ ninety-nine-cats/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+angelo = Cat.create!(
+    name: "Angelo",
+    sex: 'M',
+    birth_date: "2020/10/10",
+    color: "Black",
+    description: "I'm a cat"
+)
+carvey = Cat.create!(
+    name: "Carvey",
+    sex: 'M',
+    birth_date: "2018/10/10",
+    color: "Orange",
+    description: "I'm a dog"
+)
+thing_one = Cat.create!(
+    name: "thing_one",
+    sex: 'F',
+    birth_date: "2010/10/10",
+    color: "Grey",
+    description: "I'm an attack helicopter"
+)
+thing_two = Cat.create!(
+    name: "thing_two",
+    sex: 'M',
+    birth_date: "2000/10/10",
+    color: "White",
+    description: "I'm not a cat"
+)
