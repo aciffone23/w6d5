ff7438e89e39345eb4ba4f354137264debbec652
carveyh <carvey.hor@gmail.com>
2023-04-14 16:12:14 -0400

    Complete Cat validations:

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,11 +1,15 @@
 class Cat < ApplicationRecord
     CAT_COLORS = ["Orange", "Black", "White", "Grey", "Brown"]
-
-    validates :birth_date, :color, :name, :sex, presence: true
-    validates :color, inclusion: { in: CAT_COLORS} 
-    validates :sex, inclusion: { in: ['M', 'F']}
+    
+    validates :birth_date, :name, presence: true
+    validates :color, inclusion: { in: CAT_COLORS }, presence: true
+    validates :sex, inclusion: { in: ['M', 'F']}, presence: true
+    validate :birth_date_cannot_be_future
 
     def birth_date_cannot_be_future
-        
+        #valid bday formats "January, 1, 2022" or "MM/DD/YYYY"
+        if (self.birth_date <=> DateTime.now) == 1
+            errors.add(:birth_date, "Birthdate cannot be in the future")
+        end
     end
 end
